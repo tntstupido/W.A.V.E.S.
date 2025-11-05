a663bd1b3fa0ee6009384940aef9f2686850e5d3
mladenConcept <mladenstojanovicpo@gmail.com>
2025-11-06 00:09:12 +0100

    Prvi komit Unreal Projekat C++

---
+++ Source/WAVES/WAVES.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WAVES.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WAVES, "WAVES" );
